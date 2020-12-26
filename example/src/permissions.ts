e069f051f090af971599db25c6b946c562f399d9
Tina Holly <info@tinaciousdesign.com>
2020-12-26 02:05:33 -0800

    fix: permissions verify only read (not write)

--- example/src/permissions.ts
+++ example/src/permissions.ts
@@ -1,13 +1,12 @@
 import { PermissionsAndroid } from 'react-native';
 
 export async function hasStoragePermissions() {
-  const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
-
-  const hasPermission = await PermissionsAndroid.check(permission);
-  if (hasPermission) {
+  const readPermission = PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE;
+  const hasReadPermission = await PermissionsAndroid.check(readPermission);
+  if (hasReadPermission) {
     return true;
   }
 
-  const status = await PermissionsAndroid.request(permission);
-  return status === 'granted';
+  const readStatus = await PermissionsAndroid.request(readPermission);
+  return readStatus === 'granted';
 }
